7b717fecc3e86bdd25a08375485e75bee7c21831
sutod23 <sutod@kkszki.hu>
2025-09-19 08:35:57 +0200

    Database name is added via console.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem az adatbázis nevét: ");
+            string dbName = Console.ReadLine();
+            Connect conn = new Connect(dbName);
+        }
+    }
+}
